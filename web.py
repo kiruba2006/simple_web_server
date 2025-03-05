4a6be7c2e5579484fdd02bbe1f16337237e446ee
unknown <kirubakitu2006@gmail.com>
2025-03-05 20:31:20 +0530

    success

--- web.py
+++ web.py
@@ -1,15 +1,23 @@
-import http.server
-import socketserver
-
-PORT = 8000
-
-class MyHandler(http.server.SimpleHTTPRequestHandler):
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>My webserver</title>
+</head>
+<body>
+<h1>manasa<h1>
+</body>
+</html>
+"""
+class myhandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        print("request received")
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header('content-type', 'text/html; charset=utf-8')
         self.end_headers()
-        self.wfile.write(b"<html><body><h1>Hi Hello</h1></body></html>")
-
-with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-    print(f"Serving at port {PORT}")
-    httpd.serve_forever()
+        self.wfile.write(content.encode())
+server_address = ('',8000)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
